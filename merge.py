bb756c67426002a0cd6781a1173a2c9662fd02f2
brianbok <brianbok97@gmail.com>
2025-01-19 18:18:02 +0900

    more data

--- merge.py
+++ merge.py
@@ -1,7 +1,7 @@
 import json
 
 target_date = "2025-01" # merge target month
-days = ["04","05","17","18"]
+days = ["04","05","17","18","19"]
 result_list = []
 for day in days:
     file_path = f"/home/bokjunsoo/testspark/data/{target_date}-{day}.json"
