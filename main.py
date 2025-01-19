e3b28246dfe04b4558c4bf8e7cc917f8a9b54e3e
brianbok <brianbok97@gmail.com>
2025-01-19 17:38:41 +0900

    first spark app

--- main.py
+++ main.py
@@ -1,2 +1,10 @@
-import sys
-print(sys.path)
+from pyspark.sql import SparkSession
+spark = SparkSession.builder \
+                    .master("local") \
+                    .appName("spark_opt") \
+                    .config("spark.eventLog.enabled", "true")\
+                    .config("spark.eventLog.dir","/tmp/spark-events")\
+                    .config("spark.history.fs.logDirectory","/tmp/spark-events")
+target_file = "/opt/bitnami/spark/data/test_data.json"
+df = spark.read.format("json").option("multiLine",True).load(target_file)
+df.printSchema()
