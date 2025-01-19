c6457eb6b0e63cb9a666d2a47ecd04dd1edbb127
brianbok <brianbok97@gmail.com>
2025-01-19 17:51:36 +0900

    'g

--- jobs/main.py
+++ jobs/main.py
@@ -3,8 +3,8 @@ spark = SparkSession.builder \
                     .master("local") \
                     .appName("spark_opt") \
                     .config("spark.eventLog.enabled", "true")\
-                    .config("spark.eventLog.dir","/tmp/spark-events")\
-                    .config("spark.history.fs.logDirectory","/tmp/spark-events")\
+                    .config("spark.eventLog.dir","/opt/bitnami/spark/spark-events")\
+                    .config("spark.history.fs.logDirectory","/opt/bitnami/spark/spark-events")\
                     .getOrCreate()
 target_file = "/opt/bitnami/spark/data/test_data.json"
 df = spark.read.format("json").option("multiLine",True).load(target_file)
